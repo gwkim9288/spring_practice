7c94564388c2ec3df56bdc56f070c52a0b00708e
KimGuenWoo <slalek152@naver.com>
2021-01-27 23:13:42 +0900

    /spring practice posting ex

---
+++ ex_posting/src/main/java/com/gwkim/posting/config/RootConfig.java
@@ -0,0 +1,38 @@
+package com.gwkim.posting.config;
+
+import javax.sql.DataSource;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@MapperScan("com.gwkim.posting.mapper")
+public class RootConfig {
+	
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:3030:XE");
+		
+		hikariConfig.setUsername("gwkim");
+		hikariConfig.setPassword("gwkim");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		
+		return dataSource;
+	}
+	
+	//SqlSessionFactory 이용해서 SqlSession을 사용
+	@Bean public SqlSessionFactoryBean sqlSessionFactory() throws Exception{
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource());
+		return (SqlSessionFactoryBean)sqlSessionFactory.getObject();
+	}
+
+}
