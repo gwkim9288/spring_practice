3dfb44ad2e05404f40624370afb6fc90958e5f6b
KimGuenWoo <slalek152@naver.com>
2021-01-28 21:44:04 +0900

    /spring practice spring posing ex/ feat. CRUD

---
+++ ex_posting/src/test/java/com/gwkim/posting/mapper/BoardMapperTests.java
@@ -0,0 +1,80 @@
+package com.gwkim.posting.mapper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.gwkim.posting.domain.BoardVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {com.gwkim.posting.config.RootConfig.class})
+@Log4j
+public class BoardMapperTests {
+
+	static public Logger log = LoggerFactory.getLogger(BoardMapperTests.class.getName());
+	
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+	
+	@Test
+	public void testGetList() {
+		mapper.getList().forEach(board -> log.info(board.toString()));
+	}
+	
+	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("newbie");
+		
+		mapper.insert(board);
+		
+		log.info(board.toString());
+	}
+	
+	@Test
+	public void testInsertSelectKey() {
+		
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("newbie");
+		
+		mapper.insertSelectKey(board);
+		
+		log.info(board.toString());
+	}
+	
+	@Test
+	public void testRead() {
+		//존재하는 게시물 번호로 테스트
+		BoardVO board = mapper.read(5L);
+		
+		log.info(board.toString());
+	}
+	
+	@Test
+	public void testDelete() {
+		log.info("DELETE COUNT: " + mapper.delete(3L));
+	}
+	
+	@Test
+	public void testUpdate() {
+		
+		BoardVO board = new BoardVO();
+		
+		board.setBno(5L);
+		board.setTitle("");
+		board.setContent("");
+		board.setWriter("");
+		
+		int count = mapper.update(board);
+		log.info("UPDATE COUNT:" + count);
+	}
+}
